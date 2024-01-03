884902a0b75fac391863aa8031bfa865eb107223
Abhilash <gajulapallyabhilash@gmail.com>
2024-01-03 12:12:31 +0530

    basic server template

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const appRouter = require("./routers/appRouter");
 const app = express();
 const PORT = 8000;
 
-app.use("/",appRouter)
+app.use("/",appRouter);
 
-app.listen(8000,()=>{
+app.listen(PORT,()=>{
   console.log("Server is running");
 })
 
